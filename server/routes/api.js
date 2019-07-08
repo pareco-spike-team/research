5ec356269c17f74df677208dd9f879ac7befcd25
Cmdr Clannad <clannad@spam.org>
2019-07-08 12:29:01 +0200

    Fix expanding tag

--- server/routes/api.js
+++ server/routes/api.js
@@ -97,10 +97,10 @@ async function getArticlesWithTag(tagId) {
 	const driver = getDriver();
 	const s = driver.session();
 	const query =
-		"MATCH (article:Article)-[:Tag]->(tag:Tag) WHERE tag.id = {tagId} RETURN article, tag";
+		"MATCH (article:Article)-[:Tag]->(tag:Tag) WHERE tag.id = {tagId} RETURN tag, article";
 	const result = await runQuery(s)(query)({ tagId: tagId });
 	driver.close();
-	return result;
+	return [...mapToReturn(new Map(), result).values()];
 }
 
 const onError = (res) => err => {
