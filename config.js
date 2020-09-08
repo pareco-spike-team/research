7280b8215988aefaeace1e477c1b59964c7d4c65
Cmdr Clannad <clannad@spam.org>
2020-09-08 14:09:10 +0200

    Generate config.dev.json if its missing

--- config.js
+++ config.js
@@ -4,10 +4,48 @@ const
 	nodeEnvironment = process.env.NODE_ENV,
 	environment = process.env.ENVIRONMENT,
 	env = environment || nodeEnvironment || "dev",
+	FS = require('fs'),
 	envConfig =
 		(env === 'production') ?
 			require('./config.production.json') :
-			require('./config.dev.json');
+			getDevConfig();
+
+function getDevConfig() {
+	const fileName = `config.${env}.json`;
+	const path = `${__dirname}/${fileName}`;
+	try {
+		return JSON.parse(FS.readFileSync(path).toString());
+	}
+	catch (e) {
+		if (e.code === "ENOENT") {
+			if (env !== 'dev') { throw `Cannot find file ${fileName}`; }
+			const devConfig = {
+				"aws": {
+					"profile": "profile",
+					"account": "account",
+					"region": "region",
+					"cognito": {
+						"userPoolId": "userPoolId",
+						"appClientId": "appClientId",
+						"appClientSecret": "appClientSecret",
+						"baseUri": "baseUri"
+					}
+				},
+				"queue": {
+					"galnetArticleUpdate": "galnetArticleUpdate"
+				},
+				"neo": {
+					"url": "bolt://localhost:7689",
+					"user": "neo4j",
+					"password": "put pwd here"
+				},
+				"forceCognito": false
+			};
+			FS.writeFileSync(path, JSON.stringify(devConfig, null, '\t'));
+			return devConfig;
+		}
+	}
+}
 
 
 module.exports = (() => {
@@ -24,3 +62,4 @@ module.exports = (() => {
 		...envConfig
 	};
 })();
+
