e84b5a4c376cd569d4a937206856ce9468b1b8c1
Cmdr Clannad <clannad@spam.org>
2020-10-04 00:17:14 +0200

    POST instead of GET + some changes to create tag

--- backend/api/createTag.js
+++ backend/api/createTag.js
@@ -1,45 +1,70 @@
 'use strict';
 
 const
+	shortId = require('shortid'),
+	getArticlesWithTag = require('./getArticlesWithTag.js'),
 	{ getDriver, runQuery } = require('../util/neoHelper.js');
 
-const caseInsensitiveRegexMatch = x => `(?muis)${x.toLowerCase()}`;
 
-async function createTag(name) {
-    const driver = getDriver();
-    let s = driver.session();
-    const tagName = name ? `(?muis)${name}` : null;
-    if (!tagName) return false;
+async function addTag(session, newTag, tagId) {
+	const args = {
+		tag: newTag,
+		id: tagId
+	};
+	const addTagQuery = `MERGE (t:Tag { tag: {tag} }) ON CREATE SET t.id = {id} RETURN t`;
 
-    let addTag = `
-        MERGE (t:Tag { tag: {tag} }) ON CREATE SET t.id = {id} RETURN t
-    `;
-    let args = {
-        tag: tagName,
-        id: shortId.generate()
-    };
-    try {
-        const result = await runQuery(s)(addTag)(args);
-		console.log(`${result[0].tag} new tag created '${tagName}'`);
-        const tagArticles = `
-            MATCH (a:Article)
-            WHERE (a.text =~ {tagMatch} OR a.title =~ {tagMatch})
-                AND lower(a.title) <> 'week in review'
-                AND (NOT lower(a.title) STARTS WITH 'community goal:')
-                AND (NOT lower(a.title) STARTS WITH 'freelance report:')
-                AND (NOT lower(a.title) STARTS WITH 'a week in powerplay')
-            MATCH (t:Tag) WHERE t.tag = {tag}
-            MERGE (a)-[:Tag]->(t)
-            RETURN count(a) AS articlesUpdated`;
+	await runQuery(session)(addTagQuery)(args);
+}
 
-        const result = await runQuery(s)(tagArticles)({tagMatch: caseInsensitiveRegexMatch(tagName)});
-        
-		console.log(`${result[0].articlesUpdated} articles received new tag '${tagName}'`);
-    } catch (err) {
-        console.error(err);
-    }
-	driver.close();
-	return result;
+async function addLinkToArticle(session, articleId, tagId) {
+	const args = {
+		articleId: articleId,
+		tagId: tagId
+	};
+	const addTagQuery = `
+		MATCH (a:Article) WHERE a.id = {articleId}
+		MATCH (t:Tag) WHERE t.id = {tagId}
+		MERGE (a)-[tag:Tag]->(t)
+		RETURN a, tag, t
+		`;
+
+	await runQuery(session)(addTagQuery)(args);
+}
+
+async function addLinkToMatchingArticles(session, tagId, newTag) {
+	const args = {
+		tagId: tagId,
+		stringMatch: `(?muis)${newTag}`
+	};
+	const tagArticles = `
+		MATCH (a:Article) WHERE (a.text =~ {tagMatch}
+		MATCH (t:Tag) WHERE t.id = {tagId}
+		MERGE (a)-[:Tag]->(t)
+		RETURN count(a) AS articlesUpdated`;
+
+	return await runQuery(session)(tagArticles)(args);
+}
+
+async function createTag(newTag, articleId, addToAllArticlesMatchingTag) {
+	if (!newTag) { return false; }
+
+	const tagId = shortId.generate();
+	const driver = getDriver();
+	try {
+		const session = driver.session();
+		await addTag(session, newTag, tagId);
+		if (articleId) {
+			addLinkToArticle(session, articleId, tagId);
+		}
+		if (addToAllArticlesMatchingTag) {
+			addLinkToMatchingArticles(session, tagId, newTag);
+		}
+
+	} finally {
+		driver.close();
+	}
+
+	return await getArticlesWithTag(tagId);
 }
 
 module.exports = createTag;
--- backend/express/routes/api.js
+++ backend/express/routes/api.js
@@ -57,7 +57,6 @@ const setColor = async (req, res) => {
 	}
 };
 
-
 const removeColor = async (req, res) => {
 	const username = req.session.user.username;
 	const from = req.body.from;
@@ -71,17 +70,29 @@ const removeColor = async (req, res) => {
 	}
 };
 
+const createTag_ = async (req, res) => {
+	try {
+		const newTag = req.body.newTag;
+		const articleId = req.body.articleId;
+		const addToAllArticlesMatchingTag = req.body.addToAllArticlesMatchingTag;
+		const result = await createTag(newTag, articleId, addToAllArticlesMatchingTag);
+		res.json(result);
+	} catch (err) {
+		onError(res)(err);
+	}
+};
+
 const api = {
 	searchByTag: (req, res) => get(res, () => searchByTag(req.query.filter)),
 	getArticlesWithTag: (req, res) => get(res, () => getArticlesWithTag(req.params.tagId)),
 
 	searchArticles: (req, res) => get(res, () => searchArticles(req.query.tagFilter, req.query.articleFilter)),
 	getTagsForArticle: (req, res) => get(res, () => getTagsForArticle(req.params.articleId, req.query.includeArticles)),
+	createTag: (req, res) => createTag_(req, res),
 
 	getUser: getUser,
 	setColor: setColor,
 	removeColor: removeColor,
-	createTag: (req, res) => get(res, () => createTag(req.params.tagName)),
 };
 
 module.exports = () => {
@@ -90,12 +101,12 @@ module.exports = () => {
 		router = express.Router();
 
 	router.get('/api/tags', api.searchByTag);
+	router.post('/api/tags', api.createTag);
 	router.get('/api/tags/:tagId/articles', api.getArticlesWithTag);
 	router.get('/api/articles', api.searchArticles);
 	router.get('/api/articles/:articleId/tags', api.getTagsForArticle);
 	router.post('/api/articles/:articleId/color', api.setColor);
 	router.delete('/api/articles/:articleId/color', api.removeColor);
-	router.get('/api/tags/create', api.createTag);
 
 	router.get('/api/user', api.getUser);
 
