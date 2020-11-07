0a62596a4ed12fb85bde267c4901a94f5210c0d0
Cmdr Clannad <clannad@spam.org>
2020-11-07 15:05:42 +0100

    script

--- import.sh
+++ import.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 docker-compose down
 if [ -d ~/data/galnetpedia-neo/databases ]; then
-  rm -rf ~/data/galnetpedia-neo/databases
+  sudo rm -rf ~/data/galnetpedia-neo/databases
 fi
 docker-compose up -d
 # wait for neo to start up
 echo =Waiting for neo to startup =
 sleep 12
 echo = Importing data =
-cd dataImport
+cd ./dataImport
 node ./import.js $@
 echo = Update tags on articles =
 node ./updateArticleTags.js $@
 node ./galnetUpdate.js $@
+cd ..
